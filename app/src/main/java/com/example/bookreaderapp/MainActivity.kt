c833315a2627e2d1a425bf98ddb8fbf38f550a79
Maryam <mrymsarabi@gmail.com>
2024-06-05 23:46:01 +0330

    MainActivity - handling buttons

--- app/src/main/java/com/example/bookreaderapp/MainActivity.kt
+++ app/src/main/java/com/example/bookreaderapp/MainActivity.kt
@@ -1,15 +1,30 @@
 package com.example.bookreaderapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.bookreaderapp.activities.LoginActivity
+import com.example.bookreaderapp.activities.SignUpActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val loginButton = findViewById<Button>(R.id.login_btn)
+        val signupButton = findViewById<Button>(R.id.signup_btn)
+
+        loginButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        signupButton.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+        }
     }
 }
